e3ab4af96f8dbb591c58224412363a7320af6629
art <gulturyan.a@gmail.com>
2022-06-29 17:51:23 +0300

    sec commit

--- script1.js
+++ script1.js
@@ -1,2 +1,18 @@
 "use strict";
 
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function(){
+        console.log("Hello");
+    }
+};
+
+const john = Object.create(soldier);
+// const john = {
+//     health: 100
+//};
+
+Object.setPrototypeOf(john, soldier);
+
+john.sayHello();
